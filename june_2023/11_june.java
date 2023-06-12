4b37363ee6fe0e7ef128c342cf9874ea7c902730
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-06-12 13:35:54 +0530

    Create 11_june.java

---
+++ june_2023/11_june.java
@@ -0,0 +1,14 @@
+//Driver Code
+class Solution {
+
+    public static void update(int a[], int n, int updates[], int k)
+    {
+        // Your code goes here
+         for (int i = 0; i < k; i++) {
+            int j = updates[i];
+            for (int idx = j - 1; idx < n; idx++) {
+                a[idx]++; }
+   
+                   }
+   }
+}
