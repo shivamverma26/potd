a14fa51f605b7e0cf9ea2971e0ced538261b502a
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-07-17 11:43:43 +0530

    17_july completed

---
+++ july_2023/17_july.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String FirstNonRepeating(String stream) {
+        Map<Character, Integer> charCount = new HashMap<>();
+        Queue<Character> uniqueChars = new ArrayDeque<>();
+        StringBuilder result = new StringBuilder();
+
+        for (char ch : stream.toCharArray()) {
+            charCount.put(ch, charCount.getOrDefault(ch, 0) + 1);
+
+            if (charCount.get(ch) == 1) {
+                uniqueChars.add(ch);
+            }
+
+            while (!uniqueChars.isEmpty() && charCount.get(uniqueChars.peek()) > 1) {
+                uniqueChars.poll();
+            }
+
+            if (!uniqueChars.isEmpty()) {
+                result.append(uniqueChars.peek());
+            } else {
+                result.append('#');
+            }
+        }
+
+        return result.toString();}}
