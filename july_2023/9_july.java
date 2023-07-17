e6fc30745f721c7d65ba83eb3907b15026acf07a
Shivam Verma <94590743+shivamverma26@users.noreply.github.com>
2023-07-17 11:54:02 +0530

    9_july completed

---
+++ july_2023/9_july.java
@@ -0,0 +1,23 @@
+class Solution
+{
+    //Function to find the smallest positive number missing from the array.
+    static int missingNumber(int arr[], int size)
+    {
+       HashMap<Integer,Integer> mp=new HashMap<Integer,Integer>();
+       
+       for(int i=0;i<size;i++){
+           if(arr[i]>0)
+           mp.put(arr[i],arr[i]);
+       }
+       
+       int minPos=1;
+       while(true){
+           if(!mp.containsKey(minPos))
+           return minPos;
+           else
+           minPos++;
+       }
+        
+    }
+    
+}
